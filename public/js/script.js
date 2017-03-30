0e4ea14a5b62bbb9898327f345d0712beb02505e
Petermuturi <pmuturi65@gmail.com>
2017-03-30 17:28:46 +0300

    first command

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,19 @@
 function listen(){
-  var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
   recognition.lang = 'en-US';
   recognition.interimResults = false;
   recognition.maxAlternatives = 5;
   recognition.start();
 
   recognition.onresult = function(event) {
-      console.log('You said: ', event.results[0][0].transcript);
+      const command = event.results[0][0].transcript;
+      const action_asked = command.split(' ').shift();
+      switch (action_asked) {
+        case "Google":
+          window.open('https://google.com/search?q='+command.split(' ').slice(1).join("+"));
+          break;
+        default:
+
+      }
   }
 }
