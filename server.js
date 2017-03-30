302a907ed1b8ad7c135b5ed48cc687ba79b5e158
Petermuturi <pmuturi65@gmail.com>
2017-03-30 14:41:09 +0300

    basic setup structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+nunjucks.configure('./src', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/', (req, res)=> res.render('templates/index.njk'));
+
+app.listen(3000);
